113455b6177b97965633337dc6b2dac816618307
Agiitt <agitpnya@gmail.com>
2023-08-06 07:49:54 +0800

    run

---
+++ latihan60.py
@@ -0,0 +1,8 @@
+# pengenalan PIP
+
+# pip --version --> mengecek versi dari pip
+# pip list --> melihat pip yang sudah terinstall
+# python -m pip install --upgrade pip --> upgrade pip
+# pip install numpy --> menginstall salah satu pypi (numpy)
+# pip uninstall numpy --> menguninstall numpy
+# pip insrall numpy==1.21.0 --> mengganti numpy dari versi sekarang menjadi versi 1,21,0 
